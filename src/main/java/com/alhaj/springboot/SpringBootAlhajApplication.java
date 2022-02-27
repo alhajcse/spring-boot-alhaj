8e9bf86c6f746d7a1b64b8cfbe5e9c10218baedc
alhaj <alhajcse09@gmail.com>
2022-02-27 17:30:37 +0600

    first commit

---
+++ src/main/java/com/alhaj/springboot/SpringBootAlhajApplication.java
@@ -0,0 +1,13 @@
+package com.alhaj.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAlhajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAlhajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alhaj/springboot/SpringBootAlhajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alhaj.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAlhajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
